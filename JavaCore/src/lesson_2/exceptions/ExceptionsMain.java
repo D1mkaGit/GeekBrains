4f586c75bb5fd0104dc5155880ed5a2c3723610e
D1mkaGit <30922998+D1mkaGit@users.noreply.github.com>
2019-11-19 00:57:11 +0200

    Убрал try/catch - сократил кол-во строк.

--- JavaCore/src/lesson_2/exceptions/ExceptionsMain.java
+++ JavaCore/src/lesson_2/exceptions/ExceptionsMain.java
@@ -6,43 +6,40 @@ public class ExceptionsMain {
      * обработать возможные исключения MySizeArrayException и MyArrayDataException и вывести результат расчета.
      */
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws MyArrayDataException, MyArraySizeException {
 
-        try {
-            // correct array of symbols (not for numbers check)
-            /*System.out.println("***MyArrayDataException***");
-            new CheckExceptions().checkArray(new String[][]{
-            {"1", "2", "3", "4"},
-            {"1", "2", "/", "d"},
-            {"1", "2", "e", "f"},
-            {"1", "2", "g", "f"}});*/
+        // correct array of symbols (not for numbers check)
+        /*System.out.println("***MyArrayDataException***");
+        new CheckExceptions().checkArray(new String[][]{
+                {"1", "2", "3", "4"},
+                {"1", "2", "/", "d"},
+                {"1", "2", "e", "f"},
+                {"1", "2", "g", "f"}});*/
 
-            // incorrect array 1
-            /*System.out.println("***MyArraySizeException***");
-            new CheckExceptions().checkArray(new String[][]{
-            {"1", "2", "3", "4"},
-            {"1", "2", "c", "d"},
-            {"1", "2", "e", "f"}
-            });*/
+        // incorrect array 1
+       /* System.out.println("***MyArraySizeException***");
+        new CheckExceptions().checkArray(new String[][]{
+                {"1", "2", "3", "4"},
+                {"1", "2", "c", "d"},
+                {"1", "2", "e", "f"}
+        });*/
 
-            // incorrect array 2
-            /*System.out.println("***MyArraySizeException**");
-            new CheckExceptions().checkArray(new String[][]{
-            {"1", "2", "3", "4"},
-            {"1", "2", "c", "d"},
-            {"1", "2", "e", "f"},
-            {"1","2"}});*/
+        // incorrect array 2
+        /*System.out.println("***MyArraySizeException**");
+        new CheckExceptions().checkArray(new String[][]{
+                {"1", "2", "3", "4"},
+                {"1", "2", "c", "d"},
+                {"1", "2", "e", "f"},
+                {"1", "2"}});*/
 
-            System.out.println("***Корректный массив с суммой***");
-            new CheckExceptions().checkArray(new String[][]{
-                    {"1", "2", "3", "4"},
-                    {"1", "2", "3", "4"},
-                    {"1", "2", "3", "4"},
-                    {"1", "2", "3", "4"}
-            });
+        System.out.println("***Корректный массив с суммой***");
+        new CheckExceptions().checkArray(new String[][]{
+                {"1", "2", "3", "4"},
+                {"1", "2", "3", "4"},
+                {"1", "2", "3", "4"},
+                {"1", "2", "3", "4"}
+        });
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+
     }
 }
