6a18daa00b4dfefa30c8d1d75f6c74de407b8ba9
D1mkaGit <30922998+D1mkaGit@users.noreply.github.com>
2019-11-19 00:14:22 +0200

    Основное задание

---
+++ JavaCore/src/lesson_2/exceptions/ExceptionsMain.java
@@ -0,0 +1,48 @@
+package lesson_2.exceptions;
+
+public class ExceptionsMain {
+    /**
+     * 3. В методе main() вызвать полученный метод,
+     * обработать возможные исключения MySizeArrayException и MyArrayDataException и вывести результат расчета.
+     */
+
+    public static void main(String[] args) {
+
+        try {
+            // correct array of symbols (not for numbers check)
+            /*System.out.println("***MyArrayDataException***");
+            new Exceptions().checkArray(new String[][]{
+            {"1", "2", "3", "4"},
+            {"1", "2", "/", "d"},
+            {"1", "2", "e", "f"},
+            {"1", "2", "g", "f"}});*/
+
+            // incorrect array 1
+            /*System.out.println("***MyArraySizeException***");
+            new Exceptions().checkArray(new String[][]{
+            {"1", "2", "3", "4"},
+            {"1", "2", "c", "d"},
+            {"1", "2", "e", "f"}
+            });*/
+
+            // incorrect array 2
+            /*System.out.println("***MyArraySizeException**");
+            new Exceptions().checkArray(new String[][]{
+            {"1", "2", "3", "4"},
+            {"1", "2", "c", "d"},
+            {"1", "2", "e", "f"},
+            {"1","2"}});*/
+
+            System.out.println("***Корректный массив с суммой***");
+            new Exceptions().checkArray(new String[][]{
+                    {"1", "2", "3", "4"},
+                    {"1", "2", "3", "4"},
+                    {"1", "2", "3", "4"},
+                    {"1", "2", "3", "4"}
+            });
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
