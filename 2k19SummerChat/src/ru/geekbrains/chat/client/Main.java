506e13ddebf68e8f3ba152cc6302cd398d9d7570
D1mkaGit <30922998+D1mkaGit@users.noreply.github.com>
2019-12-10 19:22:04 +0200

    Lesson8 айлы урока

---
+++ 2k19SummerChat/src/ru/geekbrains/chat/client/Main.java
@@ -0,0 +1,23 @@
+package ru.geekbrains.chat.client;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main( String[] args ) {
+        launch(args);
+    }
+
+    @Override
+    public void start( Stage primaryStage ) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("client.fxml"));
+        primaryStage.setTitle("Geekbrains Chat");
+        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.show();
+        // primaryStage.setOnCloseRequest(event -> System.out.println("On Close"));
+    }
+}
---
+++ 2k19SummerChat/src/ru/geekbrains/chat/server/ClientHandler.java
@@ -0,0 +1,109 @@
+package ru.geekbrains.chat.server;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ClientHandler {
+    List<String> blackList;
+    private Server server;
+    private Socket socket;
+    private DataOutputStream out;
+    private DataInputStream in;
+    private String nick;
+
+    public ClientHandler( Server server, Socket socket ) {
+        try {
+            this.socket = socket;
+            this.server = server;
+            this.in = new DataInputStream(socket.getInputStream());
+            this.out = new DataOutputStream(socket.getOutputStream());
+            this.blackList = new ArrayList<>();
+            new Thread(() -> {
+                try {
+                    while (true) {
+                        String str = in.readUTF();
+                        if (str.startsWith("/auth")) { // /auth login72 pass72
+                            String[] tokens = str.split(" ");
+                            String newNick = AuthService.getNickByLoginAndPass(tokens[1], tokens[2]);
+                            if (newNick != null) {
+                                if (!server.isNickBusy(newNick)) {
+                                    sendMsg("/authok");
+                                    nick = newNick;
+                                    server.subscribe(this);
+                                    break;
+                                } else {
+                                    sendMsg("Учетная запись уже используется");
+                                }
+                            } else {
+                                sendMsg("Неверный логин/пароль");
+                            }
+                        }
+                    }
+                    while (true) {
+                        String str = in.readUTF();
+                        if (str.startsWith("/")) {
+                            if (str.equals("/end")) {
+                                out.writeUTF("/serverclosed");
+                                break;
+                            }
+                            if (str.startsWith("/w ")) { // /w nick3 lsdfhldf sdkfjhsdf wkerhwr
+                                String[] tokens = str.split(" ", 3);
+                                String m = str.substring(tokens[1].length() + 4);
+                                server.sendPersonalMsg(this, tokens[1], tokens[2]);
+                            }
+                            if (str.startsWith("/blacklist ")) { // /blacklist nick3
+                                String[] tokens = str.split(" ");
+                                blackList.add(tokens[1]);
+                                sendMsg("Вы добавили пользователя " + tokens[1] + " в черный список");
+                            }
+                        } else {
+                            server.broadcastMsg(this, nick + ": " + str);
+                        }
+                        System.out.println("Client: " + str);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    try {
+                        in.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    try {
+                        out.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    try {
+                        socket.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    server.unsubscribe(this);
+                }
+            }).start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getNick() {
+        return nick;
+    }
+
+    public boolean checkBlackList( String nick ) {
+        return blackList.contains(nick);
+    }
+
+    public void sendMsg( String msg ) {
+        try {
+            out.writeUTF(msg);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
