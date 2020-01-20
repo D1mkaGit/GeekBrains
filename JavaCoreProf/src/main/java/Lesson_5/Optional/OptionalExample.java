bb1a9d9916f256cecb20176d1507164aa6d30791
D1mkaGit <30922998+D1mkaGit@users.noreply.github.com>
2020-01-20 21:02:12 +0200

    материалы 5-ого урока

---
+++ JavaCoreProf/src/main/java/Lesson_5/Optional/OptionalExample.java
@@ -0,0 +1,65 @@
+package Lesson_5.Optional;
+
+//Optional - новый класс в пакете java.util, является контейнером (оберткой)
+//для значений которая также может безопасно содержать null.
+
+import java.util.Optional;
+
+public class OptionalExample {
+    public static void main( String[] args ) throws Exception {
+
+        // 1 объект с ненулевым значением
+//        Optional<String> name = Optional.of(null);
+//        System.out.println(name);
+
+
+        // 2 Optional.ofNullable с возможностью нулевого значения
+//        Optional<String> name2 = Optional.ofNullable(null);
+//        System.out.println(name2);
+
+
+        // 3 Optional.empty для создания пустого Optional
+//        Optional<String> emptyOptional = Optional.empty();
+//        System.out.println(emptyOptional);
+
+
+        // 4 Optional isPresent
+//        isPresent() не дает нам большой выгоды по устранению избыточности кода,
+//        но зато придает немного большую информативность написанному коду.
+        Optional<Person> person = Optional.of(new Person());
+
+//        if(person != null) {
+//            System.out.println(person);
+//        } else {
+//            System.out.println("Person not found!");
+//        }
+////////////////
+//        if (person.isPresent()) {
+//            System.out.println(person.get());
+//        } else {
+//            System.out.println("Person not found!");
+//        }
+
+
+        // 5 Optional orElse
+        // Как было раньше:
+        //  Person personNew = person != null ? person : new Person();
+        //То же самое, но с использованием Optional:
+        //   Person personNew = person.orElse(new Person());
+
+
+        // 6 Optional orElseThrow
+        // Или, если не хотим создавать объект, можно выбросить исключение:
+//        Optional<String> name = Optional.of("John"); //Optional.empty()
+//        String nameValue = name.orElseThrow(() -> new RuntimeException());
+//        System.out.println(nameValue);
+
+
+        // 7  Optional map map() — преобразовывает объект в другой объект.
+//        Optional<String> name = Optional.of("JOHN"); //Optional.empty()
+//        System.out.println(name.map(String::toLowerCase));
+
+//        Optional<Integer> age = Rep.findById(10).map(user -> user.getAge()).orElseThrow(() -> new Exception());
+//        System.out.println(age);
+    }
+}
---
+++ JavaCoreProf/src/main/java/Lesson_5/ThreadDemo.java
@@ -0,0 +1,20 @@
+package Lesson_5;
+
+public class ThreadDemo extends Thread {
+    public static void main( String[] args ) throws InterruptedException {
+        ThreadDemo t1 = new ThreadDemo();
+        ThreadDemo t2 = new ThreadDemo();
+
+        t1.setPriority(8);
+        t2.setPriority(1);
+
+        t1.start();
+        Thread.sleep(1000);
+        t2.start();
+    }
+
+    @Override
+    public void run() {
+        System.out.println("test thread " + getName());
+    }
+}
