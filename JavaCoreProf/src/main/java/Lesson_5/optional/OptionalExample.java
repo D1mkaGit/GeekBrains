cd4aa09105555fc084b3ba916617ad33b9069b4d
D1mkaGit <30922998+D1mkaGit@users.noreply.github.com>
2020-01-21 03:09:36 +0200

    материалы 5-ого урока (исправленные)

--- JavaCoreProf/src/main/java/Lesson_5/optional/OptionalExample.java
+++ JavaCoreProf/src/main/java/Lesson_5/optional/OptionalExample.java
@@ -1,6 +1,6 @@
 package lesson_5.optional;
 
-//optional - новый класс в пакете java.util, является контейнером (оберткой)
+//Optional - новый класс в пакете java.util, является контейнером (оберткой)
 //для значений которая также может безопасно содержать null.
 
 import java.util.Optional;
@@ -9,21 +9,21 @@ public class OptionalExample {
     public static void main( String[] args ) throws Exception {
 
         // 1 объект с ненулевым значением
-//        optional<String> name = optional.of(null);
+//        Optional<String> name = Optional.of(null);
 //        System.out.println(name);
 
 
-        // 2 optional.ofNullable с возможностью нулевого значения
-//        optional<String> name2 = optional.ofNullable(null);
+        // 2 Optional.ofNullable с возможностью нулевого значения
+//        Optional<String> name2 = Optional.ofNullable(null);
 //        System.out.println(name2);
 
 
-        // 3 optional.empty для создания пустого optional
-//        optional<String> emptyOptional = optional.empty();
+        // 3 Optional.empty для создания пустого Optional
+//        Optional<String> emptyOptional = Optional.empty();
 //        System.out.println(emptyOptional);
 
 
-        // 4 optional isPresent
+        // 4 Optional isPresent
 //        isPresent() не дает нам большой выгоды по устранению избыточности кода,
 //        но зато придает немного большую информативность написанному коду.
         Optional<Person> person = Optional.of(new Person());
@@ -41,25 +41,25 @@ public class OptionalExample {
 //        }
 
 
-        // 5 optional orElse
+        // 5 Optional orElse
         // Как было раньше:
         //  Person personNew = person != null ? person : new Person();
-        //То же самое, но с использованием optional:
+        //То же самое, но с использованием Optional:
         //   Person personNew = person.orElse(new Person());
 
 
-        // 6 optional orElseThrow
+        // 6 Optional orElseThrow
         // Или, если не хотим создавать объект, можно выбросить исключение:
-//        optional<String> name = optional.of("John"); //optional.empty()
+//        Optional<String> name = Optional.of("John"); //Optional.empty()
 //        String nameValue = name.orElseThrow(() -> new RuntimeException());
 //        System.out.println(nameValue);
 
 
-        // 7  optional map map() — преобразовывает объект в другой объект.
-//        optional<String> name = optional.of("JOHN"); //optional.empty()
+        // 7  Optional map map() — преобразовывает объект в другой объект.
+//        Optional<String> name = Optional.of("JOHN"); //Optional.empty()
 //        System.out.println(name.map(String::toLowerCase));
 
-//        optional<Integer> age = Rep.findById(10).map(user -> user.getAge()).orElseThrow(() -> new Exception());
+//        Optional<Integer> age = Rep.findById(10).map(user -> user.getAge()).orElseThrow(() -> new Exception());
 //        System.out.println(age);
     }
 }
