41de3944c45ad6e1a2a5202c68c10c56c9f79e9a
D1mkaGit <30922998+D1mkaGit@users.noreply.github.com>
2020-02-17 20:32:26 +0200

    Файлы второго урока

--- StarGame/core/src/ru/geekbrains/StarGame.java
+++ StarGame/core/src/ru/geekbrains/StarGame.java
@@ -1,39 +1,13 @@
 package ru.geekbrains;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.Game;
 
-public class StarGame extends ApplicationAdapter {
-    SpriteBatch batch;
-    Texture img;
-    TextureRegion region;
+import ru.geekbrains.screen.MenuScreen;
 
-    @Override
-    public void create() {
-        batch = new SpriteBatch();
-        img = new Texture("badlogic.jpg");
-        region = new TextureRegion(img, 20, 25, 150, 100);
-    }
+public class StarGame extends Game {
 
-    @Override
-    public void render() {
-        Gdx.gl.glClearColor(0.5f, 0.9f, 0.4f, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        batch.begin();
-        batch.setColor(0.1f, 0.9f, 0.5f, 0.9f);
-        batch.draw(img, 0, 0);
-        batch.setColor(0.6f, 0.3f, 0.2f, 0.3f);
-        batch.draw(region, 200, 200);
-        batch.end();
-    }
-
-    @Override
-    public void dispose() {
-        batch.dispose();
-        img.dispose();
-    }
+	@Override
+	public void create() {
+		setScreen(new MenuScreen());
+	}
 }
---
+++ StarGame/core/src/ru/geekbrains/screen/MenuScreen.java
@@ -0,0 +1,53 @@
+package ru.geekbrains.screen;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector2;
+
+import ru.geekbrains.base.BaseScreen;
+
+public class MenuScreen extends BaseScreen {
+
+    private Texture img;
+    private Vector2 touch;
+    private Vector2 v;
+    private Vector2 pos;
+
+    @Override
+    public void show() {
+        super.show();
+        img = new Texture("badlogic.jpg");
+        touch = new Vector2();
+        v = new Vector2(1f, 1f);
+        pos = new Vector2();
+    }
+
+    @Override
+    public void render(float delta) {
+        super.render(delta);
+        Gdx.gl.glClearColor(0.5f, 0.9f, 0.4f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        if ((pos.y + img.getHeight()) < Gdx.graphics.getHeight()) {
+            pos.add(v);
+        }
+        batch.begin();
+        batch.draw(img, pos.x, pos.y);
+        batch.end();
+    }
+
+    @Override
+    public void dispose() {
+        img.dispose();
+        super.dispose();
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        super.touchDown(screenX, screenY, pointer, button);
+        touch.set(screenX, Gdx.graphics.getHeight() - screenY);
+        System.out.println("touch.x = " + touch.x + " touch.y " + touch.y);
+        return false;
+    }
+
+}
