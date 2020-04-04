afe4292cdbf14a21b7046b56b107f31f9cb37b06
D1mkaGit <30922998+D1mkaGit@users.noreply.github.com>
2020-04-04 17:26:15 +0300

    дз первые 3 пункта

--- JavaAlgorithms/src/lesson_2/dz/BigArray.java
+++ JavaAlgorithms/src/lesson_2/dz/BigArray.java
@@ -1,5 +1,6 @@
 package lesson_2.dz;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 /**
@@ -9,61 +10,135 @@ import java.util.Random;
  * 4. Написать методы, реализующие рассмотренные виды сортировок, и проверить скорость выполнения каждой.
  */
 public class BigArray {
-    private final int[] arr;
-    private final int size = 1000000;
+    private final ArrayList<Integer> arr;
+    private int size = 1000000;
 
-    public static void main( String[] args ) {
+    private BigArray() {
+        this.arr = new ArrayList<>(size);
+    }
+
+    public static void main(String[] args) {
         BigArray bigArr = new BigArray(); // Создать массив большого размера (миллион элементов)
-        bigArr.fillWithRndNumbers(); //Заполнить массив случайными числами
+        bigArr.fillWithRndNumbers(); // Заполнить массив случайными числами
+
+        // негативная проверка
+        bigArr.displayById(1999999);
+        bigArr.deleteByArrElementId(1999999); // удаление по id
+        bigArr.deleteByArrValue(1999999);
+        bigArr.displayById(1999999);
+
+        // добавление данных
+        bigArr.insert(1999999, -1); //добавление числав конец массива
+        bigArr.displayById(bigArr.getSize() - 1); // смотрим последний элемнт
+        bigArr.insert(2999999, 500); //добавление числав по id
+        bigArr.displayById(500); // смотрим последний добавленный элемент
+        bigArr.displayByValue(1999999);
+
+        // удаление данных
+        bigArr.deleteByArrElementId(500); // удаление по id
+        bigArr.deleteByArrValue(1999999); //удаление по значению
+        bigArr.displayByValue(1999999);
+        bigArr.displayByValue(2999999);
+        bigArr.displayById(500);
+
+        // вывод всего массива
         bigArr.display();
     }
 
-    private BigArray() {
-        this.arr = new int[size];
+    public int getSize() {
+        return size;
     }
 
     private void display() {
         for (int i = 0; i < this.size; i++) {
-            System.out.println(i + 1 + ": " + this.arr[i]);
+            System.out.println(i + 1 + ": " + this.arr.get(i));
         }
     }
 
-    private BigArray fillWithRndNumbers() {
+    private void displayById(int elementID) {
+        if (findByArrId(elementID))
+            System.out.println("Element with is: " + elementID + " is " + this.arr.get(elementID));
+        else
+            System.out.println("Element with id: " + elementID + " not found");
+    }
+
+    private void displayByValue(int elementValue) {
+        if (findByEnumeration(elementValue))
+            System.out.println("Element with value: " + elementValue + " is found");
+        else
+            System.out.println("Element with value: " + elementValue + " not found");
+    }
+
+    private void fillWithRndNumbers() {
         Random randomNumber = new Random();
         for (int i = 0; i < this.size; i++) {
-            this.arr[i] = randomNumber.nextInt(size);
+            this.arr.add(i, randomNumber.nextInt(size));
         }
-        return this;
     }
 
-    private boolean findByEnumeration( int value ) {
+    private boolean findByEnumeration(int value) {
         for (int i = 0; i < this.size; i++) {
-            if (this.arr[i] == value) return true;
+            if (this.arr.get(i) == value) return true;
         }
         return false;
     }
 
-    private boolean binaryFind( int value ) {
-        int low = 0;
-        int high = this.size - 1;
-        int mid;
-        while (low <= high) {
-            mid = (low + high) / 2;
-            if (this.arr[mid] == value)
-                return true;
-            if (value < this.arr[mid])
-                high = mid - 1;
-            else
-                low = mid + 1;
+    private boolean findByArrId(int value) {
+        if (value >= 0 && value < this.size) {
+            int low = 0;
+            int high = this.size - 1;
+            int mid;
+            while (low <= high) {
+                mid = (low + high) / 2;
+                if (mid == value)
+                    return true;
+                if (value < mid)
+                    high = mid - 1;
+                else
+                    low = mid + 1;
+            }
         }
         return false;
     }
 
-    public void deleteByArrValue( int value ) {
-
+    public void deleteByArrValue(int value) {
+        if (findByEnumeration(value)) {
+            System.out.println("Element with value: " + value + " will be erased");
+            moveElementsDuringDelete(value);
+        } else {
+            System.out.println("Element with value: " + value + " can't be erased");
+        }
     }
 
-    public void deleteByArrElementId( int elementId ) {
+    private void deleteByArrElementId(int elementId) {
+        if (findByArrId(elementId)) {
+            System.out.println("Element " + this.arr.get(elementId) + " with id: " + elementId + " will be erased");
+            moveElementsDuringDelete(elementId);
+        } else {
+            System.out.println("Element with id: " + elementId + " can't be erased");
+        }
+    }
 
+    /**
+     * Insert values into array
+     * @param value - value to be inserted
+     * @param id    - array id, if id less than 0,value will be inserted to the end of array
+     */
+    private void insert(int value, int id) {
+        this.size++;
+        this.arr.add(this.size - 1, value);
+        if (id >= 0 && id < this.size) {
+            for (int i = this.size - 1; i > id; i--) {
+                this.arr.set(i, this.arr.get(i - 1));
+            }
+            this.arr.set(id, value);
+        }
+    }
+
+    private void moveElementsDuringDelete(int elementId) {
+        for (int i = elementId; i < this.size - 1; i++) {
+            this.arr.set(i, this.arr.get(i + 1));
+        }
+        this.size--;
     }
 }
