cfccbaa4a2b5dc723f073648a55c4ddeb4325385
D1mkaGit <30922998+D1mkaGit@users.noreply.github.com>
2020-04-24 21:42:27 +0300

    DZ 8 урока

--- JavaAlgorithms/src/lesson_8/ChainingHashMap.java
+++ JavaAlgorithms/src/lesson_8/ChainingHashMap.java
@@ -3,25 +3,15 @@ package lesson_8;
 import java.util.LinkedList;
 
 public class ChainingHashMap<Key, Value> {
-    private int capacity = 7;
+    private final int capacity = 7;
     private int size = 0;
 
-    private LinkedList<Node>[] st;
+    private final LinkedList<Node>[] st;
 
     public ChainingHashMap() {
         st = new LinkedList[capacity];
         for (int i = 0; i < st.length; i++) {
             st[i] = new LinkedList<>();
-        }
-    }
-
-    private class Node {
-        Key key;
-        Value value;
-
-        public Node(Key key, Value value) {
-            this.key = key;
-            this.value = value;
         }
     }
 
@@ -44,7 +34,7 @@ public class ChainingHashMap<Key, Value> {
         return true;
     }
 
-    public boolean contains(Key key){
+    public boolean contains(Key key) {
         return get(key) != null;
     }
 
@@ -72,15 +62,54 @@ public class ChainingHashMap<Key, Value> {
         return null;
     }
 
+    public Node delete(Key key) {
+        isKeyNotNull(key);
+        int i = hash(key);
+        for (Node node : st[i]) {
+            if (key.equals(node.key)) {
+                st[i].remove();
+                return node;
+            }
+        }
+        return null;
+    }
+
+    public Node deleteByValue(String value) {
+        for (LinkedList<Node> list : st) {
+            for (Node node : list) {
+                if (node.value.equals(value)) {
+                    st[hash(node.key)].remove();
+                    return node;
+                }
+            }
+        }
+        return null;
+    }
+
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < capacity; i++) {
             for (Node node : st[i]) {
-                sb.append(node.key + "=" + node.value+", ");
+                sb.append(node.key).append("=").append(node.value).append(", ");
             }
             sb.append("\n");
         }
         return sb.toString();
+    }
+
+    private class Node {
+        final Key key;
+        Value value;
+
+        public Node(Key key, Value value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return value.toString();
+        }
     }
 }
