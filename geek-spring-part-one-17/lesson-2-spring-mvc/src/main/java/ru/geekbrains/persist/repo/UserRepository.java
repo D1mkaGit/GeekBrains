5a7d678fabcc73de064279551f01498cc35cceb9
D1mkaGit <30922998+D1mkaGit@users.noreply.github.com>
2020-09-12 18:23:35 +0300

    Файлы четвертого урока (code refactoring)

--- geek-spring-part-one-17/lesson-2-spring-mvc/src/main/java/ru/geekbrains/persist/repo/UserRepository.java
+++ geek-spring-part-one-17/lesson-2-spring-mvc/src/main/java/ru/geekbrains/persist/repo/UserRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.geekbrains.persist.entity.User;
 
-import javax.jws.soap.SOAPBinding;
 import java.util.List;
 
 
