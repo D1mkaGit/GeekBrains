b2b216836e515e65b6ace8492820b98438d71d21
D1mkaGit <30922998+D1mkaGit@users.noreply.github.com>
2020-10-01 22:37:51 +0300

    DZ7: 1. Добавить к проекту REST API для одной из сущностей. дополнительно: - обработка ошибок - сделал вывод продуктов на ангуляре
    Вопросы:
    - как на ангуляре постраничный вывод сделать и фильтрацию данных
    - обработать ошибку при попытке создания дубликата имени, которую на уроке пропустили и почему она возникает, мы ведь не задавали уникальное для поля?

--- geek-spring-part-one-17/angularJsApp/js/app.js
+++ geek-spring-part-one-17/angularJsApp/js/app.js
@@ -1,10 +1,11 @@
 let SpringApp = angular.module('SpringApp', ['ngRoute']);
 let userList = [];
+let productList = [];
 
 SpringApp.config(function ($routeProvider, $locationProvider, $sceProvider) {
     $sceProvider.enabled(false);
     $routeProvider
-        .when('/', {
+        .when('/' || '/users', {
             templateUrl: 'users.html',
             controller: 'UserListCtrl'
         })
@@ -16,12 +17,24 @@ SpringApp.config(function ($routeProvider, $locationProvider, $sceProvider) {
             templateUrl: 'user.html',
             controller: 'UserEditCtrl'
         })
+        .when('/products', {
+            templateUrl: 'products.html',
+            controller: 'ProductListCtrl'
+        })
+        .when('/product/new', {
+            templateUrl: 'product.html',
+            controller: 'ProductNewCtrl'
+        })
+        .when('/product/:id/edit', {
+            templateUrl: 'product.html',
+            controller: 'ProductEditCtrl'
+        })
         .otherwise({
             redirectTo: '/'
         });
 });
 
-SpringApp.controller('UserListCtrl', function ($scope, $rootScope, $routeParams, $http) {
+SpringApp.controller('UserListCtrl', function ($scope, $rootScope, $routeParams, $location, $http) {
 
     $http.get("http://localhost:8080/mvc-app/api/v1/user/all")
         .then(resp => {
@@ -31,6 +44,20 @@ SpringApp.controller('UserListCtrl', function ($scope, $rootScope, $routeParams,
             resp => {
                 console.error(response.data.errorMessage);
             });
+
+    $scope.deleteUser = function (user) {
+        console.info(user);
+
+        $http.delete("http://localhost:8080/mvc-app/api/v1/user/" + user.id + "/id")
+            .then(function successCallback() {
+                alert("User has deleted Successfully");
+                const index = $scope.users.indexOf(user);
+                $scope.users.splice(index, 1);
+            }, function errorCallback(response) {
+                console.error(response.data.errorMessage);
+                alert("Error. while deleting user Try Again!");
+            });
+    }
 });
 
 SpringApp.controller('UserNewCtrl', function ($scope, $rootScope, $routeParams, $location, $http) {
@@ -60,6 +87,66 @@ SpringApp.controller('UserEditCtrl', function ($scope, $rootScope, $routeParams,
         $http.put("http://localhost:8080/mvc-app/api/v1/user", user)
             .then(resp => {
                     $location.path("/");
+                },
+                resp => {
+                    console.error(response.data.errorMessage)
+                });
+    }
+});
+
+SpringApp.controller('ProductListCtrl', function ($scope, $rootScope, $routeParams, $location, $http) {
+
+    $http.get("http://localhost:8080/mvc-app/api/v1/product/all")
+        .then(resp => {
+                productList = resp.data;
+                $scope.products = resp.data;
+            },
+            resp => {
+                console.error(response.data.errorMessage);
+            });
+
+    $scope.deleteProduct = function (product) {
+        console.info(product);
+
+        $http.delete("http://localhost:8080/mvc-app/api/v1/product/" + product.id + "/id")
+            .then(function successCallback() {
+                alert("Product has deleted Successfully");
+                const index = $scope.products.indexOf(product);
+                $scope.products.splice(index, 1);
+            }, function errorCallback(response) {
+                console.error(response.data.errorMessage);
+                alert("Error. while deleting product Try Again!");
+            });
+    }
+});
+
+SpringApp.controller('ProductNewCtrl', function ($scope, $rootScope, $routeParams, $location, $http) {
+
+    $scope.product = {id: null, title: null, cost: null};
+
+    $scope.submit = function (product) {
+        console.info(product);
+
+        $http.post("http://localhost:8080/mvc-app/api/v1/product", product)
+            .then(resp => {
+                    $location.path("/products");
+                },
+                resp => {
+                    console.error(response.data.errorMessage);
+                });
+    }
+});
+
+SpringApp.controller('ProductEditCtrl', function ($scope, $rootScope, $routeParams, $location, $http) {
+
+    $scope.product = productList.find(prd => prd.id === parseInt($routeParams.id));
+
+    $scope.submit = function (product) {
+        console.info(product);
+
+        $http.put("http://localhost:8080/mvc-app/api/v1/product", product)
+            .then(resp => {
+                    $location.path("/products");
                 },
                 resp => {
                     console.error(response.data.errorMessage)
