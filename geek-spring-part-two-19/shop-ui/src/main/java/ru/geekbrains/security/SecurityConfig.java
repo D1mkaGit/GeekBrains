0114a23939e59b45813b3e7c556aa8da883327d9
D1mkaGit <30922998+D1mkaGit@users.noreply.github.com>
2020-11-04 23:03:43 +0200

    DZ3: - Имплементация логина и логаута

--- geek-spring-part-two-19/shop-ui/src/main/java/ru/geekbrains/security/SecurityConfig.java
+++ geek-spring-part-two-19/shop-ui/src/main/java/ru/geekbrains/security/SecurityConfig.java
@@ -42,6 +42,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .formLogin()
                 .loginPage("/login")
                 .loginProcessingUrl("/authenticateTheUser")
+                .defaultSuccessUrl("/", true)
                 .permitAll()
                 .and()
                 .logout()
