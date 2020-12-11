757cd7e278d38d308c223dd0ccfe8c594a94f09f
D1mkaGit <30922998+D1mkaGit@users.noreply.github.com>
2020-12-11 05:06:31 +0200

    DZ7: CartService unit tests (with problems in code)

--- geek-spring-part-two-19/shop-ui/src/test/java/ru/geekbrains/service/CartServiceTest.java
+++ geek-spring-part-two-19/shop-ui/src/test/java/ru/geekbrains/service/CartServiceTest.java
@@ -6,7 +6,8 @@ import ru.geekbrains.controller.repr.ProductRepr;
 import ru.geekbrains.service.model.LineItem;
 
 import java.math.BigDecimal;
-import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -15,9 +16,13 @@ public class CartServiceTest {
 
     private CartService cartService;
     private BigDecimal bidPrice;
+    private ProductRepr expectedProduct;
+    Map<LineItem, BigDecimal> lineItems;
+    LineItem lineItem;
 
     @BeforeEach
     public void init() {
+        bidPrice = new BigDecimal(123);
         cartService = new CartServiceImpl();
     }
 
@@ -29,22 +34,68 @@ public class CartServiceTest {
 
     @Test
     public void testAddOneProduct() {
-        bidPrice = new BigDecimal(123);
-        ProductRepr expectedProduct = new ProductRepr();
-        expectedProduct.setId(1L);
-        expectedProduct.setName("Product name");
-
-        cartService.addProductBidPrice(expectedProduct, bidPrice);
-
-        List<LineItem> lineItems = cartService.getLineItems();
-        assertNotNull(lineItems);
-        assertEquals(1, lineItems.size());
-
-        LineItem lineItem = lineItems.get(0);
+        createTestProductWithDefaultParams();
+        addTestProductToCart();
         assertEquals(bidPrice, lineItem.getBidPrice());
-
         assertEquals(expectedProduct.getId(), lineItem.getProductId());
         assertNotNull(lineItem.getProductRepr());
         assertEquals(expectedProduct.getName(), lineItem.getProductRepr().getName());
+    }
+
+    @Test
+    public void testRemoveProduct(){
+        createTestProductWithDefaultParams();
+        addTestProductToCart();
+        cartService.removeProduct(lineItem);
+        assertEquals(0, lineItems.size());
+    }
+
+    @Test
+    public void testUpdateCart(){
+        createTestProductWithDefaultParams();
+        addTestProductToCart();
+        BigDecimal newBidPrice = new BigDecimal(321);
+        cartService.updateCart(lineItem);
+        assertNotNull(lineItem.getBidPrice());
+        assertNotNull(lineItems.keySet().iterator().next());
+        assertEquals(newBidPrice, lineItem.getBidPrice());
+    }
+
+    @Test
+    public void testGetLineItems(){
+        createTestProductWithDefaultParams();
+        cartService.addProductBidPrice(expectedProduct, bidPrice);
+        lineItems = cartService.getLineItems().stream().collect(Collectors.toMap(li -> li, LineItem::getBidPrice));
+        assertNotNull(lineItems);
+        assertEquals(1, lineItems.size());
+    }
+
+    @Test
+    public void testGetSubTotal(){
+        createTestProductsWithParams(1L, "Test Product 1");
+        addTestProductToCart();
+        createTestProductsWithParams(2L, "Test Product 2");
+        addTestProductToCart();
+        BigDecimal subTotalPrice = cartService.getSubTotal();
+        assertNotNull(subTotalPrice);
+        BigDecimal sumOfPricesInCart = bidPrice.add(bidPrice);
+        assertNotNull(sumOfPricesInCart);
+        assertEquals(subTotalPrice,sumOfPricesInCart);
+    }
+
+    private void createTestProductWithDefaultParams() {
+        createTestProductsWithParams(1L,"Product name");
+    }
+
+    private void createTestProductsWithParams(Long id, String name) {
+        expectedProduct = new ProductRepr();
+        expectedProduct.setId(id);
+        expectedProduct.setName(name);
+    }
+
+    private void addTestProductToCart() {
+        cartService.addProductBidPrice(expectedProduct, bidPrice);
+        lineItems = cartService.getLineItems().stream().collect(Collectors.toMap(li -> li, LineItem::getBidPrice));
+        lineItem = lineItems.keySet().iterator().next();
     }
 }
