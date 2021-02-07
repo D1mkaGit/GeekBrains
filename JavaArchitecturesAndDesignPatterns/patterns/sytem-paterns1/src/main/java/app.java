99d5c14b44fb3deecc88b0357158b00ecc03e42b
D1mkaGit <30922998+D1mkaGit@users.noreply.github.com>
2021-02-08 00:00:39 +0200

    DZ6: 1. Для всех объектов, которые непосредственно хранятся в БД, реализовать шаблон Data Mapper.

---
+++ JavaArchitecturesAndDesignPatterns/patterns/sytem-paterns1/src/main/java/app.java
@@ -0,0 +1,63 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class app {
+    private static Connection connection;
+
+    public static void connect() {
+        try {
+            String url = "jdbc:mysql://localhost:3306/archpatsys1";
+            String username = "test";
+            String password = "test";
+
+            System.out.println("Connecting database...");
+
+            if (connection == null) {
+                try {
+                    Class.forName("com.mysql.jdbc.Driver");
+                    connection = DriverManager.getConnection(url, username, password);
+                    System.out.println("Database connected!");
+                } catch (ClassNotFoundException | SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        } catch (Exception e) {
+            throw new IllegalStateException("Cannot connect the database!", e);
+        }
+    }
+
+    public static void disconnect() {
+        if (connection != null) {
+            try {
+                connection.close();
+                connection = null;
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws SQLException {
+        connect();
+
+        ClientMapper clientMapper = new ClientMapper(connection);
+
+        // используем
+        Client client = clientMapper.findById(1L);
+        System.out.println(client);
+
+        Client cl1 = new Client("testLogin", "testPassword", "testName", "testSurname", "test@test.ee");
+        clientMapper.insert(cl1);
+        cl1.setId(clientMapper.findIdByLogin(cl1.getLogin())); // вытаскиваем id созданного
+        System.out.println(clientMapper.findById(cl1.getId()));
+
+        cl1.setFirstName("changedName");
+        clientMapper.update(cl1);
+        System.out.println(clientMapper.findById(cl1.getId()));
+
+        clientMapper.delete(cl1);
+
+        disconnect();
+    }
+}
